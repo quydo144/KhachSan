b8903b80a0a97014c6f6a7fda5eeba13cce56d48
Nguyen Chi Thong <Nguyen Chi Thong@CHITHONG>
2019-10-09 13:14:35 +0700

    123

---
+++ UI/frmDichVu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Home
+{
+    public partial class frmDichVu : DevExpress.XtraEditors.XtraUserControl
+    {
+        public frmDichVu()
+        {
+            InitializeComponent();
+        }
+    }
+}
