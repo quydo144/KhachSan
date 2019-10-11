ac1354b0ac1ad562bb42f382eab49ebe382f15ff
quydo144 <36450461+quydo144@users.noreply.github.com>
2019-10-11 18:34:53 +0700

    phong

---
+++ UI/frmPhong.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Home
+{
+    partial class frmPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonEdit1 = new DevExpress.XtraEditors.SpinEdit();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.maPhong = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.tenPhong = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.tang = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.tinhTrang = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.loaiPhong = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonEdit1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1055, 305);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(720, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Cập nhật";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(735, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(265, 30);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(198, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(265, 30);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(735, 92);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(265, 30);
+            this.textBox5.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(604, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 38);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Tình trạng";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(604, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 38);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Loại phòng";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(604, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 38);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ghi chú";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(69, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 38);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tầng";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(69, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 38);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên phòng";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(69, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã phòng";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonEdit1
+            // 
+            this.buttonEdit1.EditValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.buttonEdit1.Location = new System.Drawing.Point(198, 96);
+            this.buttonEdit1.Name = "buttonEdit1";
+            this.buttonEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.buttonEdit1.Properties.EditValueChangedFiringMode = DevExpress.XtraEditors.Controls.EditValueChangedFiringMode.Default;
+            this.buttonEdit1.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.None;
+            this.buttonEdit1.Size = new System.Drawing.Size(265, 22);
+            this.buttonEdit1.TabIndex = 4;
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 305);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(1055, 429);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.maPhong,
+            this.tenPhong,
+            this.tang,
+            this.tinhTrang,
+            this.loaiPhong});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // maPhong
+            // 
+            this.maPhong.Caption = "Mã phòng";
+            this.maPhong.MinWidth = 25;
+            this.maPhong.Name = "maPhong";
+            this.maPhong.Visible = true;
+            this.maPhong.VisibleIndex = 0;
+            this.maPhong.Width = 94;
+            // 
+            // tenPhong
+            // 
+            this.tenPhong.Caption = "Tên phòng";
+            this.tenPhong.MinWidth = 25;
+            this.tenPhong.Name = "tenPhong";
+            this.tenPhong.Visible = true;
+            this.tenPhong.VisibleIndex = 1;
+            this.tenPhong.Width = 94;
+            // 
+            // tang
+            // 
+            this.tang.Caption = "Tầng";
+            this.tang.MinWidth = 25;
+            this.tang.Name = "tang";
+            this.tang.Visible = true;
+            this.tang.VisibleIndex = 2;
+            this.tang.Width = 94;
+            // 
+            // tinhTrang
+            // 
+            this.tinhTrang.Caption = "Tình trạng";
+            this.tinhTrang.MinWidth = 25;
+            this.tinhTrang.Name = "tinhTrang";
+            this.tinhTrang.Visible = true;
+            this.tinhTrang.VisibleIndex = 3;
+            this.tinhTrang.Width = 94;
+            // 
+            // loaiPhong
+            // 
+            this.loaiPhong.Caption = "Loại phòng";
+            this.loaiPhong.MinWidth = 25;
+            this.loaiPhong.Name = "loaiPhong";
+            this.loaiPhong.Visible = true;
+            this.loaiPhong.VisibleIndex = 4;
+            this.loaiPhong.Width = 94;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(735, 52);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(265, 30);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // frmPhong
+            // 
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1055, 734);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPhong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phòng";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private DevExpress.XtraEditors.SpinEdit buttonEdit1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn maPhong;
+        private DevExpress.XtraGrid.Columns.GridColumn tenPhong;
+        private DevExpress.XtraGrid.Columns.GridColumn tang;
+        private DevExpress.XtraGrid.Columns.GridColumn tinhTrang;
+        private DevExpress.XtraGrid.Columns.GridColumn loaiPhong;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
